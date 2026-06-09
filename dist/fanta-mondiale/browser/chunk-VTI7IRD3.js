fba9b7e48ed2f004bb2be91ac763722ababb0a3c
Catix86 <c.miky86@libero.it>
2026-06-09 10:38:34 +0200

    fs

--- dist/fanta-mondiale/browser/chunk-VTI7IRD3.js
+++
@@ -1,1 +0,0 @@
-var n={EXACT_RESULT:10,CORRECT_OUTCOME:4,CHAMPION_WINNER:100};function r(e,t){return e>t?"1":e<t?"2":"X"}function o(e){return Date.now()>=e.getTime()-300*1e3}export{n as a,r as b,o as c};
